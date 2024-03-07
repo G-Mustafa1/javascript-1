2acf78e2b0e930aa29814a379a075716b7378b1a
G-Mustafa1 <157616503+G-Mustafa1@users.noreply.github.com>
2024-03-08 03:27:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+var name1 = "JavaScript Alert\nError! Please enter a valid password";
+alert(name1);
+
+alert("JavaScript Alert\nWelcome to JS Land... \nHappy Codind!");
+
+alert("JavaScript Alert\nWelcome to JS Land...");
+
+var name2 = "JavaScript Alert\nHappy Coding! \n⬜ prevent this page from creating additional dialogs.";
+// var name3 = ⬜ prevent this page from creating additional dialogs.
+
+alert(name2);
+
+alert("JavaScript Alert\nHello...I can run JS through my web browser's console");
